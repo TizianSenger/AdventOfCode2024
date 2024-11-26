a1d39c4809786b1313c80966ea31cc2626811cd0
SETI157 <tizian.senger@de.airbus.com>
2024-11-26 10:23:32 +0100

    Added day1 filereader

--- Day1/Day1_Question1.py
+++ Day1/Day1_Question1.py
@@ -1,1 +1,9 @@
 print("Day1 Question1")
+
+def readDataFromFile():
+    file = open("AdventOfCode2024\Day1\source.txt", "r")
+    content = file.read()
+    print(content)
+    file.close()
+
+readDataFromFile()
