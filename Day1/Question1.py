e30b3366bdcb08a4b80aad48ccfd72c33d5f7227
TizianSenger <tiziansengerbw@gmail.com>
2024-12-02 09:05:09 +0100

    First Day finished

--- Day1/Question1.py
+++ Day1/Question1.py
@@ -1,9 +1,48 @@
 print("Day1 Question1")
 
+inputData = []
+leftList = []
+rightList= []
+listOfDifs = []
+
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
+    global inputData
+    file = open("Day1\source.txt", "r")
     content = file.read()
-    print(content)
+    content = content.split("\n")
+    for element in content:
+        splitElement = element.split(" ")
+        while "" in splitElement:
+            splitElement.remove("")
+        inputData.append(splitElement)
     file.close()
 
+def prepareData():
+    global leftList
+    global rightList
+    for element in inputData:
+        leftList.append(element[0])
+        rightList.append(element[1])
+    leftList.sort()
+    rightList.sort()
+
+def makeCalculations():
+    global listOfDifs
+    counter = 0
+    while counter < len(leftList):
+        numberLeft = int(leftList[counter])
+        numberRight = int(rightList[counter])
+        if numberLeft >= numberRight:
+            listOfDifs.append(numberLeft-numberRight)
+        else:
+            listOfDifs.append(numberRight-numberLeft)
+        counter = counter + 1
+    sumOFDifs = 0
+    for element in listOfDifs:
+        sumOFDifs = sumOFDifs + element
+    print(sumOFDifs)
+
 readDataFromFile()
+prepareData()
+makeCalculations()
+
--- Day1/Question2.py
+++ Day1/Question2.py
@@ -1,10 +1,43 @@
 print("Day1 Question2")
 
+inputData = []
+leftList = []
+rightList= []
+
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
+    global inputData
+    file = open("Day1\source.txt", "r")
     content = file.read()
-    print(content)
+    content = content.split("\n")
+    for element in content:
+        splitElement = element.split(" ")
+        while "" in splitElement:
+            splitElement.remove("")
+        inputData.append(splitElement)
     file.close()
 
+def prepareData():
+    global leftList
+    global rightList
+    for element in inputData:
+        leftList.append(element[0])
+        rightList.append(element[1])
+    leftList.sort()
+    rightList.sort()
+
+def makeCalculations():
+    sumOfSimCount = 0
+    counter = 0
+    while counter < len(leftList):
+        simCount = 0
+        for element in rightList:
+            if leftList[counter] == element:
+                simCount = simCount + 1
+        sumOfSimCount = sumOfSimCount + int(leftList[counter]) * simCount
+        counter = counter + 1
+    print(sumOfSimCount)
+
 readDataFromFile()
-print("Test")
+prepareData()
+makeCalculations()
+
