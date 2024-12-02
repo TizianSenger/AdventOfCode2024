fbe805aceb88da04ea983386b63a9862fd338f84
TizianSenger <tiziansengerbw@gmail.com>
2024-12-02 10:20:24 +0100

    day2 finished

--- Day2/Question1.py
+++ Day2/Question1.py
@@ -1,9 +1,39 @@
-print("Day1 Question1")
+print("Day2 Question1")
+
+listOfListsData = []
+filteredAscDesc = []
+minDif = 1
+maxDif = 3
 
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
-    content = file.read()
-    print(content)
-    file.close()
+    global listOfListsData
+    file = open(r"Day2/source.txt", "r")
+    content = file.read().splitlines()
+    for element in content:
+        tempArr = list(map(int, element.split()))
+        listOfListsData.append(tempArr)
+
+def distance(a, b):
+    return abs(a - b)
+
+def checkForOrder():
+    global filteredAscDesc
+    for entry in listOfListsData:
+        if all(entry[i] <= entry[i + 1] for i in range(len(entry) - 1)) or all(entry[i] >= entry[i + 1] for i in range(len(entry) - 1)):
+            filteredAscDesc.append(entry)
+
+def makeCalculations():
+    countOfValid = 0
+    for entry in filteredAscDesc:
+        valid = True
+        for i in range(len(entry) - 1):
+            if not (minDif <= distance(entry[i], entry[i + 1]) <= maxDif):
+                valid = False
+                break
+        if valid:
+            countOfValid = countOfValid + 1
+    print(countOfValid)
 
 readDataFromFile()
+checkForOrder()
+makeCalculations()
--- Day2/Question2.py
+++ Day2/Question2.py
@@ -1,9 +1,45 @@
-print("Day1 Question2")
+print("Day2 Question2")
+
+listOfListsData = []
+minDif = 1
+maxDif = 3
 
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
-    content = file.read()
-    print(content)
+    global listOfListsData
+    file = open(r"Day2/source.txt", "r")
+    for line in file:
+        listOfListsData.append(list(map(int, line.split())))
     file.close()
 
+def distance(a, b):
+    return abs(a - b)
+
+def signum(n):
+    return (n > 0) - (n < 0)
+
+def is_safe(report, bad_level_tolerated, signum_value):
+    for i in range(1, len(report)):
+        diff = report[i] - report[i - 1]
+
+        if signum(diff) != signum_value or distance(report[i - 1], report[i]) > maxDif:
+            if bad_level_tolerated:
+                return False
+
+            # entfene element try
+            return (
+                is_safe(report[:i] + report[i + 1:], True, signum_value) or
+                is_safe(report[:i - 1] + report[i:], True, signum_value)
+            )
+    return True
+
+def makeCalculations():
+    countOfValid = 0
+    for report in listOfListsData:
+        # test sicher entweder asc oder desc
+        if is_safe(report, False, 1) or is_safe(report, False, -1):
+            countOfValid += 1
+
+    print(countOfValid)
+
 readDataFromFile()
+makeCalculations()
