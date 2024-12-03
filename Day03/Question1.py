cb616276bd8f317f961d535dffc3db285e7889c0
TizianSenger <141033214+TizianSenger@users.noreply.github.com>
2024-12-03 10:18:31 +0100

    regex is shit

--- Day03/Question1.py
+++ Day03/Question1.py
@@ -1,9 +1,34 @@
+import re
 print("Day1 Question1")
 
+scrambledString = ""
+multiplicants = []
+regexPattern = r'mul\(\d+,\d+\)'
+
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
+    global scrambledString
+    file = open("AdventOfCode2024\Day03\source.txt", "r")
     content = file.read()
-    print(content)
+    scrambledString = content
     file.close()
 
+def cleanExtractData():
+    global multiplicants
+    extractedMuls = re.findall(regexPattern, scrambledString)
+    for element in extractedMuls:
+        element = element.replace("mul(", "")
+        element = element.replace(")", "")
+        element = element.split(",")
+        element[0] = int(element[0])
+        element[1] = int(element[1])
+        multiplicants.append(element)
+
+def clacData():
+    endValue = 0
+    for element in multiplicants:
+        endValue = endValue + element[0]*element[1]
+    print(endValue)
+
 readDataFromFile()
+cleanExtractData()
+clacData()
--- Day03/Question2.py
+++ Day03/Question2.py
@@ -1,9 +1,43 @@
-print("Day1 Question2")
+import re
+print("Day1 Question1")
+
+scrambledString = ""
+multiplicants = []
+regexPattern = r'mul\(\d+,\d+\)'
+
+print("regex is complete shit just dont use it i hate my life")
 
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
+    global scrambledString
+    file = open("AdventOfCode2024\Day03\source.txt", "r")
     content = file.read()
-    print(content)
+    scrambledString = content
     file.close()
 
+def cleanExtractData():
+    global multiplicants
+    extractedMuls = re.findall(regexPattern, scrambledString)
+    enabled = True
+    for element in extractedMuls:
+        if "do()" in element:
+            enabled = True
+            element = element.replace("do()", "")
+        elif "dont()" in element:
+            enabled = False
+        if enabled == True:
+            element = element.replace("mul(", "")
+            element = element.replace(")", "")
+            element = element.split(",")
+            element[0] = int(element[0])
+            element[1] = int(element[1])
+            multiplicants.append(element)
+
+def clacData():
+    endValue = 0
+    for element in multiplicants:
+        endValue = endValue + element[0]*element[1]
+    print(endValue)
+
 readDataFromFile()
+cleanExtractData()
+clacData()
