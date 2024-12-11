3e1e894a2f62f5e1cf3d590985158769c670ba1a
TizianSenger <141033214+TizianSenger@users.noreply.github.com>
2024-12-11 14:26:29 +0100

    asdf

--- Day11/Question1.py
+++ Day11/Question1.py
@@ -1,9 +1,46 @@
-print("Day1 Question1")
+print("Day11 Question1")
+
+inputArr = []
+blinks = 25
 
 def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
+    global inputArr
+    file = open("AdventOfCode2024\Day11\source.txt", "r")
     content = file.read()
-    print(content)
+    inputArr = content.split(" ")
+    # print(content)
     file.close()
+    
+def rearangeAndRecalcStones(counter):
+    global inputArr
+    tempArr = []
+    print("Stones getting rearanged " + str(counter))
+    for element in inputArr:
+        if element == "0":
+            element = "1"
+            tempArr.append(element)
+        elif len(element) % 2 == 0:
+            middle = len(element)/2
+            leftHalf = element[:int(middle)]
+            rightHalf = element[int(middle):]
+            # print(leftHalf + "|" + rightHalf)
+            element[0:int(middle)]
+            tempArr.append(str(int(leftHalf)))
+            tempArr.append(str(int(rightHalf)))
+        else:
+            tempArr.append(str(int(element) * 2024))
+    inputArr = tempArr
+    # print(inputArr)
+    
 
+def calculateForBilnks():
+    print("calculating stones for " +  str(blinks) + " blinks")
+    counter = 0
+    while counter < blinks:
+        counter = counter + 1     
+        rearangeAndRecalcStones(counter)
+    print(str(len(inputArr)) + " Stones")
+    
 readDataFromFile()
+calculateForBilnks()
+
--- Day11/Question2.py
+++ Day11/Question2.py
@@ -1,9 +1,62 @@
-print("Day1 Question2")
+data = "9694820 93 54276 1304 314 664481 0 4"
 
-def readDataFromFile():
-    file = open("AdventOfCode2024\Day1\source.txt", "r")
-    content = file.read()
-    print(content)
-    file.close()
+dict = {}
 
-readDataFromFile()
+# def transform_stones(input):
+#     output = []
+#     stones = input
+#     for stone in stones:
+#         if stone == "0":
+#             output.append("1")
+#         elif len(stone) % 2 == 0:
+#             output.append(str(int(stone[0:int(len(stone)/2)])))
+#             output.append(str(int(stone[int(len(stone)/2):])))
+#         else:
+#             output.append(str(int(stone) * 2024))
+#     return output
+
+def rules(input, iterations, max_iterations):
+    key = str(input) + "_" + str(iterations) + "_" + str(max_iterations)
+    if key in dict:
+        return dict[key]
+    if iterations == max_iterations + 1:
+        return len(input)
+
+    score = 0
+    for stone in input:
+        if stone == "0":
+            score += rules(["1"], iterations + 1, max_iterations)
+        elif len(stone) % 2 == 0:
+            score += rules([str(int(stone[0:int(len(stone)/2)]))], iterations + 1, max_iterations)
+            score += rules([str(int(stone[int(len(stone)/2):]))], iterations + 1, max_iterations)
+        else:
+            score += rules([str(int(stone) * 2024)], iterations + 1, max_iterations)
+
+    # print(key, score)
+    dict[key] = score
+    return score
+
+def transform_stones2(input, iterations, max_iterations):
+    score = 0
+    for stone in input:
+        score += rules([stone], iterations, max_iterations)
+    return score
+
+input = data.split(" ")
+
+
+print(transform_stones2(input, 1, 25))
+print(transform_stones2(input, 1, 75))
+
+
+# print(input)
+# for i in range(75):
+#     input = transform_stones2(input)
+#     print(i, len(input))
+#     # print(input)
+
+
+
+# print(input, len(input))
+# print(len(input))
+#
